f13d0a4ca2d62abfe1cf835de4470885fae17e4b
aarcThom <thomas@aarc.io>
2023-03-24 14:16:08 -0700

    Chain functions to the book prototype using Book.protype

--- object-constructors.js
+++ object-constructors.js
@@ -1,24 +1,25 @@
 /* Making a constructor for a book object. The object should have the book's title, author, number of pages, 
 and whether or not the book has been read */
 
+let myLibrary = [];
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-
-    const readStatus = () => this.read ? 'this book has been read' : 'this book aint been read';
-
-    this.info = function Info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus()}`;
+    this.read = read;  
     }
+
+// toggling the read property
+Book.prototype.isRead = function() {
+    this.read = !this.read
 }
 
+// returns the book info
+Book.prototype.Info = function() {
+    const readStatus = () => this.read ? 'this book has been read' : 'this book aint been read';
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus()}`;
+}
 
-const daHobbit = new Book('The Hobbit', 'Some Guy', '999', false);
-
-console.log(daHobbit.info());
-
-daHobbit.read = true;
-
-console.log(daHobbit.info());
+const daHobbit = new Book('hobbit', 'jr tokin', '69', false);
