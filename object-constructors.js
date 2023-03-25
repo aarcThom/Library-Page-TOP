e1c3b77c3673a2f40a2e82947c9e24cde1945e09
aarcThom <thomas@aarc.io>
2023-03-24 17:19:36 -0700

    Figure out book add function and event listener
    So it turns out that if you want to use variables in a function triggered
    by an eventListener, you have to wrap said function in an anonymous
    function. Strange....

--- object-constructors.js
+++ object-constructors.js
@@ -1,8 +1,8 @@
+// grabbing global variables
+const docRoot = document.querySelector(':root');
+
 /* Making a constructor for a book object. The object should have the book's title, author, number of pages, 
 and whether or not the book has been read */
-
-let myLibrary = [];
-
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -22,8 +22,34 @@ Book.prototype.Info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus()}`;
 }
 
-function AddBookToLibrary(bTitle, bAuthor, bPages,bRead) {
-    myLibrary.push(Book(bTitle, bAuthor, bPages, bRead))
+// Adds the book to the screen library
+Book.prototype.ScreenInfo = function() {
+    const newSBook = document.createElement('div');
+    newSBook.textContent = this.Info();
+    newSBook.className = 'book';
+    return newSBook;
 }
 
-const daHobbit = new Book('hobbit', 'jr tokin', '69', false); 
+// the main library grid contrainer
+const screenLibrary = document.getElementById('library_panel');
+
+let myLibrary = [
+    new Book('The Hobbit', 'J.R. Tolkien', 100, false),
+    new Book('A Dog Book', "Ruff Mcbark", 77, true),
+    new Book('The Big Book of Kavorka', "Anne M.L. Lure", 99, false)
+];
+
+function AddBookToLibrary(bTitle, bAuthor, bPages,bRead) {
+    let newBook = new Book(bTitle, bAuthor, bPages, bRead);
+    myLibrary.push(newBook);
+    screenLibrary.appendChild(newBook.ScreenInfo());
+}
+
+// the add book button
+const newBookButton = document.querySelector('#add_button');
+newBookButton.addEventListener("click", function(){AddBookToLibrary('yo','yo',69,false);});
+newBookButton.onclick = () => console.log(myLibrary);
+
+
+
+
